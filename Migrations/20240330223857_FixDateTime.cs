acaeeeec160ed8b486c7f74fc8676d16500a2d55
CaueMondek <cauemondekk@gmail.com>
2024-03-31 20:36:09 -0300

    :white_check_mark: Migrations update

---
+++ Migrations/20240330223857_FixDateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DOTNET_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Todos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DateTime",
+                table: "Todos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+    }
+}
