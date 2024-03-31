6770850bc797d95cb3cbd0668700b53d1b8d08dd
CaueMondek <cauemondekk@gmail.com>
2024-03-31 20:33:04 -0300

    :wrench: chore: Configure the builder and run app

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using DOTNET_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
