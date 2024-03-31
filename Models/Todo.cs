37a1c03765ab361a82e8538f49fbce762b452deb
CaueMondek <cauemondekk@gmail.com>
2024-03-31 20:24:22 -0300

    :sparkles: feat: ToDo Model, with ID, Title, Done, and Date

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -7,5 +7,9 @@ namespace DOTNET_API.Models
 {
     public class Todo
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
     }
 }
